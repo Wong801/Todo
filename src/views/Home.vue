bba4a5657f6f5c463b3609bd6812e2b1ebb7602c
Catur Satrio <52611164+Wong801@users.noreply.github.com>
2021-06-30 18:27:25 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,5 @@
       <router-link to="/todo" class="text-black">Simple Todo App</router-link> ||
       <router-link to="/hope" class="text-black">Wall Of Hopes</router-link>
     </div>
-    <img src="https://asset.indosport.com/article/image/q/80/282577/dota_22-169.jpg?w=750&h=423" alt="">
   </div>
 </template>
